0bfb1b27a636dd1738b19f20d229530a85ec1fef
SoraKumo <info@croud.jp>
2026-02-18 14:25:51 +0900

    Update IntelliSense build path and clean up Emscripten JS proxy.

--- src/cpp/satoru_proxy.js
+++ src/cpp/satoru_proxy.js
@@ -1,9 +1,8 @@
 mergeInto(LibraryManager.library, {
-  saveSetjmp: function(env, label, stackPtr) {
-    // 最新の Emscripten では内部的な setjmp 処理に委譲
+  saveSetjmp: function (env, label, stackPtr) {
     return _emscripten_setjmp(env, label, stackPtr);
   },
-  testSetjmp: function(env, label) {
+  testSetjmp: function (env, label) {
     return _emscripten_test_setjmp(env, label);
-  }
+  },
 });
